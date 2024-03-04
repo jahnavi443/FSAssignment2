6fdcc36002697e5a8f0e2ea568001eedde6513c2
jahnavi443 <141243760+jahnavi443@users.noreply.github.com>
2024-03-04 10:29:13 +0530

    Create course.service.ts

---
+++ course.service.ts
@@ -0,0 +1,6 @@
+export class CoursesService{
+    getCourses(){
+        return ["C: C is a structured programming language","Python: Python is an interpreted language","Java: Java is platform-independent Language",
+        "C++: C++ is a procedural language"];
+    }
+}
